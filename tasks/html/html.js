27aaf0a9325be68f7b6199de2e33cf79e335c54d
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-05-26 09:47:16 +0300

    add browser-sync reloading

--- tasks/html/html.js
+++ tasks/html/html.js
@@ -1,10 +1,12 @@
 import gulp from 'gulp'
 import include from 'gulp-file-include'
 import replace from 'gulp-replace'
+import browserSync from 'browser-sync'
 
 export const html = () => {
   return gulp.src('./src/index.html')
     .pipe(include())
     .pipe(replace(/@assets\//g, 'assets/'))
     .pipe(gulp.dest('./build/'))
+    .pipe(browserSync.stream())
 }
--- tasks/js/js.js
+++ tasks/js/js.js
@@ -2,10 +2,12 @@ import gulp from "gulp"
 import webpack from "webpack-stream"
 import rename from "gulp-rename"
 import { config } from './../../webpack.config.js'
+import browserSync from "browser-sync"
 
 export const js = () => {
   return gulp.src('./src/js/app.js', { sourcemaps: true })
     .pipe(webpack(config))
     .pipe(rename('app.js'))
     .pipe(gulp.dest('./build/js'))
+    .pipe(browserSync.stream())
 }
