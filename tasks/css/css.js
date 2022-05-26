5c0a28d6a377adb3341f4afc683c939586ab29f9
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-05-26 09:43:03 +0300

    add gulp-group-css-media-queries

--- tasks/css/css.js
+++ tasks/css/css.js
@@ -6,6 +6,7 @@ import replace from 'gulp-replace'
 import autoPrefixer from 'gulp-autoprefixer'
 import clean from 'gulp-clean-css'
 import rename from 'gulp-rename'
+import groupMediaQueries from 'gulp-group-css-media-queries'
 
 const sass = gulpSass(dartSass)
 
@@ -15,6 +16,7 @@ export const css = () => {
     .pipe(sass({ 
       outputStyle: 'expanded'
     }))
+    .pipe(groupMediaQueries())
     .pipe(autoPrefixer({
       grid: true,
       cascade: true,
